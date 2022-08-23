8a0f7a7d2c976c82c9cc7ba827fb265c947d7497
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2022-08-23 08:40:16 -0300

    up 1016

---
+++ Iniciante Java/bee1016.java
@@ -0,0 +1,11 @@
+// 1016 - Distância
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int d,min;
+        Scanner sc = new Scanner(System.in);
+        d = sc.nextInt();
+        min = d * 2;
+        System.out.printf("%d minutos\n", min);
+    }
+}
